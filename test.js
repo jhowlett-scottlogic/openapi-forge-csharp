edfde96123188102444a23d85944a217d7f4d743
jhowlett-scottlogic <jhowlett@scottlogic.com>
2022-09-13 11:12:57 +0100

    refactor test.js

--- test.js
+++ test.js
@@ -5,14 +5,16 @@ const shell = require("shelljs");
 const clArgs = process.argv.slice(2);
 
 // Retrieve the path to feature paths from cl arguments of 'npm test', use default value if none given
-featurePath = clArgs[0] || "..\\..\\..\\openapi-forge\\features\\\*.feature";
+const featurePath = clArgs[0] || "..\\..\\..\\openapi-forge\\features\\\*.feature";
 
-const originalFile = fs.readFileSync("./tests/FeaturesTests/FeaturesTests.csproj", "utf-8");
+const projectPath = "./tests/FeaturesTests/FeaturesTests.csproj";
 
-// Replace file path to .feature files in .csproj file
-fs.writeFileSync("./tests/FeaturesTests/FeaturesTests.csproj", originalFile.replace("FEATURE_PATH", featurePath));
+const originalFile = fs.readFileSync(projectPath, "utf-8");
 
-shell.exec(`dotnet test ./tests/FeaturesTests/FeaturesTests.csproj`);
+// Replace file path to .feature files in .csproj file, use handlebars style to help make the search value unique
+fs.writeFileSync(projectPath, originalFile.replace("{{FEATURE_PATH}}", featurePath));
 
-// Revert .csproj file back
-fs.writeFileSync("./tests/FeaturesTests/FeaturesTests.csproj", originalFile);
+shell.exec(`dotnet test ${projectPath}`);
+
+// Revert .csproj file back to original
+fs.writeFileSync(projectPath, originalFile);
